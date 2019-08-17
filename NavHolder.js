edf9bb670c5be3fc04970608376c8988d31b0776
Erik Grubbs <erikgrubbs@gmail.com>
2019-08-17 14:41:26 -0700

    [prog] Staged for merging

---
+++ NavHolder.js
@@ -0,0 +1,10 @@
+import { createStackNavigator } from 'react-navigation';
+import Home from './HomePage';
+import Event from './EventPage';
+
+import { createStackNavigator } from 'react-navigation';
+
+export default createStackNavigator({
+  Home,
+  Event
+})
